0876c903bf8a84781b36ba810c785d368e1853ab
CJavatX <cdpm98@hotmail.com>
2025-12-29 21:00:49 -0600

    SECCIÓN 9 - TERMINADO

---
+++ 04-country-app/src/app/country/components/country-search-input/country-search-input.ts
@@ -0,0 +1,11 @@
+import { Component, input, output } from '@angular/core';
+
+@Component({
+  selector: 'country-search-input',
+  imports: [],
+  templateUrl: './country-search-input.html',
+})
+export class CountrySearchInput {
+  value = output<string>();
+  placeholder = input<string>();
+}
